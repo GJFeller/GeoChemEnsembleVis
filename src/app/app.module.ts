483e2029a551a360a394db29c68f8cf40a915878
Gustavo Feller <fellergustavo@gmail.com>
2017-11-01 18:39:44 -0200

    Test at home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WindowComponent } from './window/window.component';
 import * as $ from 'jquery';
-//import 'jquery-ui';
 
 
 @NgModule({
